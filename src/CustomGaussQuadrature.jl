31af9c5d8df5f7b2d391d4c1adfe5f0b0047f2e7
Paul Kabaila <P.Kabaila@latrobe.edu.au>
2025-06-09 13:51:30 +1000

    Comments added

--- src/CustomGaussQuadrature.jl
+++ src/CustomGaussQuadrature.jl
@@ -2,22 +2,122 @@ module CustomGaussQuadrature
 # Written by Dr. Paul Kabaila, 
 # Department of Mathematical and Physical Sciences,
 # La Trobe University, Melbourne, Australia.
-# February 2025.
+# June 2025.
 # 
 # This module consists of the following Julia scripts:
 #
 # (1) gauss_quad_moment_dets_scr.jl
-# This script is for computing the custom-made Gauss 
-# quadrature nodes and weights using a moment-based method 
-# via moment determinants to compute the coefficients
-# in the three-term recurrence relation, using
-# the type T, which is specified by the user. 
-# This is followed by the use of the package
-# GenericLinearAlgebra.jl
+# This script is for computing the custom-made Gauss quadrature 
+# rule with n nodes using a moment-based method via moment 
+# determinants. This is done using the following two steps:
+#
+# Step 1: Compute the recursion coefficients 
+# [α₀, α₁, ..., αₙ₋₁] and [β₁, ..., βₙ] of the 
+# three-term recurrence relation using a moment-based 
+# method via moment determinants. The aim of 
+# Step 1 is to compute approximations to the 
+# vectors [a₁, ..., aₙ] = [α₀, ... , αₙ₋₁] and 
+# [b₁, ..., bₙ₋₁] = [√β₁, ... , √βₙ₋₁] with maximum
+# absolute errors and maximum relative errors,
+# respectively, bounded above by 10⁻¹⁸.
+# The map from the vector of moments
+# [μ₀, μ₁, ..., μ₂ₙ₋₁] to the vector 
+# [α₀, α₁, ..., αₙ₋₁,β₁, ..., βₙ] of recursion 
+# coefficients is severely ill-conditioned. 
+# This limitation is overcome by using BigFloat
+# arithmetic with carefully chosen precision.
+#
+# Step 2: Compute the Gauss quadrature rule with n nodes
+# from the eigenvalues and eigenvectors of the symmetric 
+# tridiagonal Jacobi matrix Jₙ with main diagonal
+# [a₁, ..., aₙ] = [α₀, ... , αₙ₋₁] and subdiagonal 
+# [b₁, ..., bₙ₋₁] = [√β₁, ... , √βₙ₋₁], using  
+# Double64 arithmetic. The computation of these 
+# eigenvalues and eigenvectors is carried out using
+# the package GenericLinearAlgebra.jl
 #
 # (2) inner_prod_fns_scr.jl
-# This script ...
-
+# This script is for computing an approximation to the inner 
+# product of two functions u: ℝ → ℝ and v: ℝ → ℝ. 
+# Let f: ℝ → [0,∞) denote a specified nonnegative weight function. 
+# The inner product of the two functions u and v is defined to be
+#
+#    ∞
+#    ∫ u(x) v(x) f(x) dx.                                  
+#   -∞
+#
+# To compute this inner product let the function g = uv, so that
+# this integral becomes 
+# 
+#    ∞
+#    ∫ g(x) f(x) dx.                                       
+#   -∞
+#
+# We also suppose that the support of the weight function f
+# is an interval with lower and upper endpoints a and b, 
+# respectively. Here -∞ ≤ a < b ≤ ∞. Therefore this integral
+# becomes
+# 
+#    b
+#    ∫ g(x) f(x) dx.                                      (1) 
+#    a
+#
+# To compute this integral, we use the transformation described
+# on p.94 of Gautschi (2004). In other words, we transform the  
+# support interval with lower and upper endpoints a and b, 
+# respectively, to the interval [-1,1] using the 
+# transformation
+#
+#   b                1
+#   ∫ g(x) f(x) dx = ∫ g(ϕ(y)) f(ϕ(y)) ϕ′(y) dy              (2)
+#   a               -1
+#
+# where  
+#
+#        ϕ(y) = (1/2) (b - a) y + (1/2) (b + a) if -∞ < a < b < ∞
+#        
+#        ϕ(y) = b - (1 - y) / (1 + y)           if -∞ = a < b < ∞
+#   
+#        ϕ(y) = a + (1 + y) / (1 - y)           if -∞ < a < b = ∞
+#
+#        ϕ(y) = y / (1 - y²)                    if -∞ = a < b = ∞
+#
+# The integral on the right-hand side of (1) is approximated using Gauss
+# Legendre quadrature with r nodes. 
+#
+# (3) gauss_quad_stjieltjes_scr.jl
+# This script is for computing the custom-made Gauss quadrature 
+# rule with n nodes using the Stjieltjes procedure.
+# This is done using the following two steps:
+#
+# Step 1: Compute the recursion coefficients 
+# [α₀, α₁, ..., αₙ₋₁] and [β₁, ..., βₙ] of the 
+# three-term recurrence relation using the 
+# Stjieltjes procedure, where the inner product 
+# of two functions is found using the script 
+# inner_prod_fns_scr.jl. The aim of 
+# Step 1 is to compute approximations to the 
+# vectors [a₁, ..., aₙ] = [α₀, ... , αₙ₋₁] and 
+# [b₁, ..., bₙ₋₁] = [√β₁, ... , √βₙ₋₁] with maximum
+# absolute errors and maximum relative errors,
+# respectively, bounded above by 10⁻¹⁸.
+#
+# Step 2: Compute the Gauss quadrature rule with n nodes
+# from the eigenvalues and eigenvectors of the symmetric 
+# tridiagonal Jacobi matrix Jₙ with main diagonal
+# [a₁, ..., aₙ] = [α₀, ... , αₙ₋₁] and subdiagonal 
+# [b₁, ..., bₙ₋₁] = [√β₁, ... , √βₙ₋₁], using  
+# Double64 arithmetic. The computation of these 
+# eigenvalues and eigenvectors is carried out using
+# the package GenericLinearAlgebra.jl
+#
+# (4) gauss_quad_utilities_scr.jl
+# This script consists of some utility functions for the
+# Julia package CustomGaussQuadrature.jl
+#
+# Reference
+# Gautschi, W. (2004) Orthogonal Polynomials, Computation and 
+# Approximation. Oxford University Press, Oxford.
 
 using GaussQuadrature
 using FastGaussQuadrature
