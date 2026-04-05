e31d9c63821d2322226e1fb977492d9e441ad300
Paul Kabaila <p.kabaila@latrobe.edu.au>
2026-04-06 07:46:45 +1000

    tiny improvement in introductory comments

--- test/test_stieltjes_lnf_new_scr.jl
+++ test/test_stieltjes_lnf_new_scr.jl
@@ -24,7 +24,7 @@
 #     CustomGaussQuadrature - Julia package\CustomGaussQuadrature\
 #
 # Step 2 — Start the Julia REPL:
-#     CTRL + Shift + P  >  Julia: Start REPL
+#     Ctrl+Shift+P  (to bring up Command Palette)>  Julia: Start REPL
 #
 # Step 3 — Activate the local package:
 #     julia> ]
@@ -43,7 +43,7 @@
 #------------------------------------------------------------
 #
 # Step 1 — Start the Julia REPL:
-#     CTRL + Shift + P  >  Julia: Start REPL
+#     Ctrl+Shift+P  (to bring up Command Palette)>  Julia: Start REPL
 #
 # Step 2 — Activate a temporary environment, install the package,
 #           and load it:
@@ -70,7 +70,7 @@
 #------------------------------------------------------------
 #
 # Step 1 — Start the Julia REPL:
-#     CTRL + Shift + P  >  Julia: Start REPL
+#     Ctrl+Shift+P  (to bring up Command Palette)>  Julia: Start REPL
 #
 # Step 2 — Install the package and load it:
 #     julia> using Pkg
